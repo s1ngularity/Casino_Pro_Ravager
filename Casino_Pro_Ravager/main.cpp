8137be59a013e1769f55b853f35bb7dab607a6bd
Alex Vaskov <avask@MacBook-Pro-Alex.local>
2017-02-12 15:51:26 +0300

    Initial Commit

---
+++ Casino_Pro_Ravager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Casino_Pro_Ravager
+//
+//  Created by Alex Vaskov on 12.02.17.
+//  Copyright © 2017 AV_Tests. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
